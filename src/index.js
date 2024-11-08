6682615b41fd39f92ce924417048251e2bb97bb0
Josh Nash <ustaticdb@gmail.com>
2024-11-08 20:11:41 +0100

    outdated packages nvm use 22.11.0

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import App from "./App";
 import "./index.css";
 
-// const container = document.getElementById("root");
-// const root = createRoot(container);
+const container = document.getElementById("root");
+const root = createRoot(container);
 
-// root.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// );
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
-ReactDOM.render(
+/* ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
 );
+ */
